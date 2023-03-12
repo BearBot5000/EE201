3305ace203fe76184d34e7b93c2de6b21fdd7bc5
BearBot5000 <122122612+BearBot5000@users.noreply.github.com>
2023-03-12 16:10:10 -0700

    Update exercise_4.ino
    updated to not use display module

--- exercise_4.ino
+++ exercise_4.ino
@@ -1,30 +1,34 @@
-# include <SegmentDisplay.h>;
+
+int DELAY = 1500;
+
+byte nums[10] = {64, 121, 36, 48, 25, 18, 2, 120, 0, 16};
+int start_pin = 2;
+int num_disp = 9;
 
 void setup() {
-  SegmentDisplay segmentDisplay(2, 3, 4, 5, 6, 7, 8, 9);
-  //segmentDisplay.testDisplay();
-  segmentDisplay.displayHex(9, false);
-  delay (1500);
-  segmentDisplay.displayHex(8, false);
-  delay (1500);
-  segmentDisplay.displayHex(7, false);
-  delay (1500);
-  segmentDisplay.displayHex(6, false);
-  delay (1500);
-  segmentDisplay.displayHex(5, false);
-  delay (1500);
-  segmentDisplay.displayHex(4, false);
-  delay (1500);
-  segmentDisplay.displayHex(3, false);
-  delay (1500);
-  segmentDisplay.displayHex(2, false);
-  delay (1500);
-  segmentDisplay.displayHex(1, false);
-  delay (1500);
-  segmentDisplay.displayHex(0, false);
-  delay (1500);
+  Serial.begin(9600);
+  for(int i = start_pin; i < 7 + start_pin; i++) {
+    pinMode(i, OUTPUT);
+  }
 }
 
 void loop() {
-  
+  lightSevenSeg(nums[ num_disp ]);
+   num_disp --;
+  if( num_disp  < 0) {
+     num_disp  = 9;
+  }
+  delay(DELAY);
+}
+
+void lightSevenSeg(byte values) {
+    byte values_temp = values;
+    for(int i = 0; i < 7; i++) {
+        if(values_temp % 2 == 0) {
+            digitalWrite(start_pin + i, LOW);
+        } else {
+            digitalWrite(start_pin + i, HIGH);
+        }
+        values_temp = values_temp >> 1;
+    }
 }
